30e33e21b88a9893bc11c9489c1f1d2e05862632
anDack <1160083806@qq.com>
2017-01-04 23:58:48 +0800

    增加设置函数

--- app/src/main/java/com/andack/urgencysecretcode/LocationUtils.java
+++ app/src/main/java/com/andack/urgencysecretcode/LocationUtils.java
@@ -1,5 +1,10 @@
 package com.andack.urgencysecretcode;
 
+import android.content.Context;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+
 /**
  * Created by anDack on 2017/1/4.
  * 邮箱：1160083806@qq.com
@@ -29,4 +34,20 @@ package com.andack.urgencysecretcode;
  */
 
 public class LocationUtils {
+
+    private static AMapLocationClientOption getLocationOption() {
+        AMapLocationClientOption mOption=new AMapLocationClientOption();
+        mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mOption.setGpsFirst(false);
+        mOption.setHttpTimeOut(30000);
+        mOption.setInterval(2000);
+        mOption.setNeedAddress(true);
+        mOption.setOnceLocation(false);
+        mOption.setOnceLocationLatest(false);
+        AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.HTTP);
+        mOption.setSensorEnable(false);//可选，设置是否使用传感器。默认是false
+        mOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        mOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+        return mOption;
+    }
 }
