f5dd9039773a08388b47edad903235ca2197c9d8
anDack <1160083806@qq.com>
2017-01-04 20:41:29 +0800

    增加了简单的UI

--- app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
+++ app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
@@ -2,12 +2,23 @@ package com.andack.urgencysecretcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button accept_btn;
+    private EditText sender_et;
+    private EditText sendContent_et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        accept_btn= (Button) findViewById(R.id.acceptBtn);
+        sender_et= (EditText) findViewById(R.id.senderEd);
+        sendContent_et= (EditText) findViewById(R.id.contentEd);
     }
 }
