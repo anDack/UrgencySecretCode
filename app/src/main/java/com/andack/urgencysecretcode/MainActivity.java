5ccb4f946a160392c6032707c8ec1c9d10cda22b
anDack <1160083806@qq.com>
2017-01-04 22:21:34 +0800

    关于SharePreferencesTools类我搞错了

--- app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
+++ app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
@@ -1,19 +1,49 @@
 package com.andack.urgencysecretcode;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private Button accept_btn;
     private EditText sender_et;
     private EditText sendContent_et;
+    private SharePreferencesTools sharePreferencesTools;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        sharePreferencesTools=new SharePreferencesTools(this);
+        accept_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setData();
+            }
+        });
+
+    }
+
+    private void setData() {
+        String sender=sender_et.getText().toString();
+        String content=sendContent_et.getText().toString();
+//        sharePreferencesTools.isSharedPre();
+        if (!sender.equals(null)&& !content.equals(null))
+        {
+            sharePreferencesTools.setSharedPre(sender,content);//设置sender和content
+
+            Log.i(TAG, "setData: "+ sharePreferencesTools.getSender());
+        }else {
+            Toast.makeText(MainActivity.this, R.string.input_note,Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void initView() {
---
+++ app/src/main/java/com/andack/urgencysecretcode/SharePreferencesTools.java
@@ -0,0 +1,91 @@
+package com.andack.urgencysecretcode;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Created by anDack on 2017/1/4.
+ * 邮箱：1160083806@qq.com
+ * <p>                      _oo0oo_
+ * <p>                   o8888888o
+ * <p>                    88" . "88
+ * <p>                   (| -_- |)
+ * <p>                    0\  =  /0
+ * <p>                  ___/`---'\___
+ * <p>                .' \\|     |// '.
+ * <p>               / \\|||  :  |||// \
+ * <p>              / _||||| -:- |||||- \
+ * <p>             |   | \\\  -  /// |   |
+ * <p>             | \_|  ''\---/''  |_/ |
+ * <p>             \  .-\__  '-'  ___/-. /
+ * <p>           ___'. .'  /--.--\  `. .'___
+ * <p>        ."" '<  `.___\_<|>_/___.' >' "".
+ * <p>       | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ * <p>       \  \ `_.   \_ __\ /__ _/   .-` /  /
+ * <p>   =====`-.____`.___ \_____/___.-`___.-'=====
+ * <p>                     `=---='
+ * <p>
+ * <p>
+ * <p>   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * <p>
+ * <p>            佛祖保佑         永无BUG
+ */
+
+public class SharePreferencesTools {
+    private static final String TAG = "Tools";
+    private Context mContext;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    public SharePreferencesTools(Context context)
+    {
+        this.mContext=context;
+        this.sharedPreferences=mContext.getSharedPreferences("data",Context.MODE_PRIVATE);
+
+        this.editor=sharedPreferences.edit();
+        Log.i(TAG, "SharePreferencesTools: shareIs create");
+    }
+
+    /**
+     * 是否有创建SharePreferences
+     * @return true 已经存在 false 不存在需要新建
+     */
+    public boolean isSharedPre()
+    {
+        boolean isShare=false;
+        if(!sharedPreferences.getString("sender",null).equals(null))
+        {
+            isShare=true;
+        }
+        return  isShare;
+    }
+
+    /**
+     * 用来重置数据
+     */
+    public void restSharedPre()
+    {
+
+        editor.putString("sender",null);
+        editor.putString("content",null);
+        editor.commit();
+    }
+
+    /**
+     * 用来设置数据
+     * @param sender 设置发送的电话号码
+     * @param content 设置发送的内容
+     */
+    public void setSharedPre(String sender,String content)
+    {
+        editor.putString("sender",sender);
+        editor.putString("content",content);
+        editor.commit();
+    }
+    public String getSender()
+    {
+        String sender=sharedPreferences.getString("sender",null);
+        return sender;
+    }
+
+}
