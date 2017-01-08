54b513155fcbafdba2fff3453a77471acb7068f3
anDack <1160083806@qq.com>
2017-01-08 09:51:51 +0800

    添加recyclerView

--- app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
+++ app/src/main/java/com/andack/urgencysecretcode/MainActivity.java
@@ -1,15 +1,22 @@
 package com.andack.urgencysecretcode;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 public class MainActivity extends CheckPermissionsActivity {
     private static final String TAG = "MainActivity";
-    private Button accept_btn;
-    private EditText sender_et;
-    private EditText sendContent_et;
+//    private Button accept_btn;
+//    private EditText sender_et;
+//    private EditText sendContent_et;
+    private RecyclerView recyclerView;
     private SharePreferencesTools sharePreferencesTools;
 
     @Override
@@ -17,13 +24,14 @@ public class MainActivity extends CheckPermissionsActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
         sharePreferencesTools=new SharePreferencesTools(this);
-        accept_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setData();
-            }
-        });
+//        accept_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setData();
+//            }
+//        });
 
     }
 
@@ -32,23 +40,38 @@ public class MainActivity extends CheckPermissionsActivity {
         super.onDestroy();
     }
 
-    private void setData() {
-        String sender=sender_et.getText().toString();
-        String content=sendContent_et.getText().toString();
-        if (!sender.equals(null)&& !content.equals(null))
-        {
-            sharePreferencesTools.setSharedPre(sender,content);//设置sender和content
-            Log.i(TAG, "setData: "+ sharePreferencesTools.getSender());
-        }else {
-            Toast.makeText(MainActivity.this, R.string.input_note,Toast.LENGTH_SHORT).show();
-        }
-
-    }
+//    private void setData() {
+//        String sender=sender_et.getText().toString();
+//        String content=sendContent_et.getText().toString();
+//        if (!sender.equals(null)&& !content.equals(null))
+//        {
+//            sharePreferencesTools.setSharedPre(sender,content);//设置sender和content
+//            Log.i(TAG, "setData: "+ sharePreferencesTools.getSender());
+//        }else {
+//            Toast.makeText(MainActivity.this, R.string.input_note,Toast.LENGTH_SHORT).show();
+//        }
+//
+//    }
 
     private void initView() {
-        accept_btn= (Button) findViewById(R.id.acceptBtn);
-        sender_et= (EditText) findViewById(R.id.senderEd);
-        sendContent_et= (EditText) findViewById(R.id.contentEd);
+//        accept_btn= (Button) findViewById(R.id.acceptBtn);
+//        sender_et= (EditText) findViewById(R.id.senderEd);
+//        sendContent_et= (EditText) findViewById(R.id.contentEd);
+        recyclerView= (RecyclerView) findViewById(R.id.recycler);
+//        String[] mData=new String[]{"暗码1","暗码2","暗码3","暗码4"};
+        ArrayList<String> mData=new ArrayList<String>();
+        setData(mData);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerAdapter Adapter =new recyclerAdapter(this,mData);
+        recyclerView.setAdapter(Adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
+    public void setData(ArrayList<String> data) {
+        data.add("暗码1");
+        data.add("暗码2");
+        data.add("暗码3");
+        data.add("暗码4");
+    }
 }
---
+++ app/src/main/java/com/andack/urgencysecretcode/recyclerAdapter.java
@@ -0,0 +1,73 @@
+package com.andack.urgencysecretcode;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by anDack on 2017/1/8.
+ * 邮箱：1160083806@qq.com
+ * <p>                      _oo0oo_
+ * <p>                   o8888888o
+ * <p>                    88" . "88
+ * <p>                   (| -_- |)
+ * <p>                    0\  =  /0
+ * <p>                  ___/`---'\___
+ * <p>                .' \\|     |// '.
+ * <p>               / \\|||  :  |||// \
+ * <p>              / _||||| -:- |||||- \
+ * <p>             |   | \\\  -  /// |   |
+ * <p>             | \_|  ''\---/''  |_/ |
+ * <p>             \  .-\__  '-'  ___/-. /
+ * <p>           ___'. .'  /--.--\  `. .'___
+ * <p>        ."" '<  `.___\_<|>_/___.' >' "".
+ * <p>       | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ * <p>       \  \ `_.   \_ __\ /__ _/   .-` /  /
+ * <p>   =====`-.____`.___ \_____/___.-`___.-'=====
+ * <p>                     `=---='
+ * <p>
+ * <p>
+ * <p>   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * <p>
+ * <p>            佛祖保佑         永无BUG
+ */
+
+public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.myViewHolder> {
+    private Context mContext;
+    private List<String> mDatas;
+    private LayoutInflater inflater;
+    public recyclerAdapter(Context context, List<String>mData)
+    {
+        this.mContext=context;
+        this.mDatas=mData;
+        this.inflater=LayoutInflater.from(mContext);
+    }
+    @Override
+    public myViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.item,parent,false);
+        myViewHolder holder=new myViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(myViewHolder holder, int position) {
+        holder.tv.setText(mDatas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+    class myViewHolder extends RecyclerView.ViewHolder{
+        TextView tv;
+        public myViewHolder(View itemView) {
+            super(itemView);
+            tv= (TextView) itemView.findViewById(R.id.itemTv);
+        }
+    }
+}
